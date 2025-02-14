fb5e95a26dc87aff5a3355712d0951af477f18a4
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-02-14 10:13:47 +0100

    Prøver å få trekkvinduet til å fungere

--- Scenes/move_window.gd
+++ Scenes/move_window.gd
@@ -4,14 +4,17 @@ var all_moves = [["d4","d5"],["c4","dxc4"]]
 # change variable to the one inside _ready() when things are updated
 # Might need to have it signal each time a move is made
 
+
 func _ready() -> void:
+	var rtl = $"."
+
 	# var all_moves = <name of variable>.<name of source node>
-	_display_moves(all_moves) 
+	rtl.text += _display_moves(all_moves)
 
-
-func _display_moves(moves: Array) -> void:
+func _display_moves(moves: Array):
 	var moves_as_string = ""
 	for moveset in moves:
-		moves_as_string += "%d,%d\n" % [moveset[0], moveset[1]]
+		if moveset.size() > 2:
+			moves_as_string += "%d,%d\n" % [moveset[0], moveset[1]]
 		
-	text = moves_as_string
+	return moves_as_string
